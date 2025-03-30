d8b34f08dbf803a99957b7b3d0ffd53e7e5c13b8
D3vlin <alexisdq16@gmail.com>
2025-03-30 10:43:29 -0500

    Control Props

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
+
+type ToggleProps = {
+  isToggle?: boolean
+  onToggle?: (value: boolean) => void
+}
+
+export const Toggle: React.FC<ToggleProps> = ({isToggle = false, onToggle}) => {
+  const [internalToggle, setInternalToggle] = useState(isToggle)
+
+  const toggle = () => {
+    const newToggle = !internalToggle
+    setInternalToggle(newToggle)
+
+    if (onToggle) {
+      onToggle(newToggle)
+    }
+  }
+  return (
+    <button onClick={toggle}>
+      {internalToggle ? "ON 😁": "OFF 😑"}
+    </button>
+  );
+};
+
 export const ParentComponent = () => {
+  const [toggleState, setToggleState] = useState(false)
+
   return (
     <div>
+      <p>Toggle Is {toggleState ? "ON 😁": "OFF 😑"}</p>
+      <Toggle isToggle={toggleState} onToggle={setToggleState} />
     </div>
   );
 };
