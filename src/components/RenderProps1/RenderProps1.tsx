2f6f60378573004a2333cf6deec662afdaf6129a
D3vlin <alexisdq16@gmail.com>
2025-03-30 10:10:51 -0500

    Render props and composition

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,20 @@
-export const ParentComponent = () => {
+type ChildComponent = {
+  render: (name: string) => JSX.Element
+}
+
+export const ChildComponent: React.FC<ChildComponent> = ({ render }) => {
+  const name = "DevlinCode"
+
   return (
-    <>
-    </>
+    <div>
+      {render(name)}
+    </div>
   );
 };
+
+export const ParentComponent = () => {
+  return (
+    <ChildComponent render={(name) => <p>Hello, {name} !</p>} />
+  );
+};
+
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,13 @@
-const ParentComponent = () => {
+import { useState } from "react";
+
+interface ParentComponentProps {
+    render: (data: string[]) => React.ReactNode
+}
+
+const ParentComponent: React.FC<ParentComponentProps> = ({render}) => {
+    const [data] = useState<string[]>(["Sad 😢", "Happy 😊", "Cool 😎"])
+
+    return <ul>{render(data)}</ul>
 };
 
 export default ParentComponent;
