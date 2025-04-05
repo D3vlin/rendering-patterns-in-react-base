388b98b426bdb7e9041daee7feab3c31dbd038e3
D3vlin <alexisdq16@gmail.com>
2025-04-05 16:35:59 -0500

    Controlled and uncontrolled components

--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,16 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const handleSubmit = () =>{
+    if(inputRef.current) {
+      alert(`Nuevo producto en el carrito: ${inputRef.current.value} ✔`)
+    }
+  }
   return (
     <div>
+      <input type="text" placeholder="Nombre del producto. Ej: Manzana" ref={inputRef} />
+      <button onClick={handleSubmit}>Añadir al carrito</button>
     </div>
   );
 };
